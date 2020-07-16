e7765630fd02bb3b38e9f344aba609be038025dd
me-abhinav-1001 <65178804+me-abhinav-1001@users.noreply.github.com>
2020-07-16 18:18:12 +0530

    updating database url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-concentric-00117',
         user: 'postgres',
         password: 'anand1234',
         database: 'smartbrain'
